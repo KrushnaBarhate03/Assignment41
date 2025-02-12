f2049c228d0a9e837304d2d7526cc2320005807b
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-02-12 13:22:12 +0530

    Assignment 41 completed

--- src/index.js
+++ src/index.js
@@ -1,43 +1,36 @@
-import {createRoot} from "react-dom/client";
-
-import { ProductCard } from "./Productcard";
-
+import { createRoot } from "react-dom/client";
+import {ProductCard} from './ProductCard'; 
+ 
 
 const Container = {
   display: "flex",
-  flexWrap:"wrap",
+  flexWrap: "wrap",
   justifyContent: "center",
-}
+};
 
 const headStyle = {
   textAlign: "center",
-}
+};
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <>
- 
-  <div style={Container}>
-  </div>
-
-  <h1 style={headStyle}>Mobile Collection</h1>
-  <div style={Container}>
-  <ProductCard
-    Image = "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/r/b/j/-original-imah3agqepywexex.jpeg?q=70"
-
-    description = "Samsung Galaxy F15 5G"
-    price = "16,999"
- 
-   
-  />
-
-  <ProductCard
-    Image = "https://image-us.samsung.com/SamsungUS/home/mobile/phones/pf/galaxy-a/03032023/1.jpg?$product-details-jpg$" 
-    description = "Galaxy S22 | Galaxy S22+ "
-    price = "51,000"
-   
-  />
-  </div>
+    <h1 style={headStyle}>Mobile Collection</h1>
+    <div style={Container}>
+      <ProductCard
+        Image="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/r/b/j/-original-imah3agqepywexex.jpeg?q=70"
+        description="Samsung Galaxy F15 5G"
+        price="16,999"
+      />
+  
+      <ProductCard
+        Image="https://image-us.samsung.com/SamsungUS/home/mobile/phones/pf/galaxy-a/03032023/1.jpg?$product-details-jpg$"
+        
+        description="Galaxy S22 | Galaxy S22+"
+        price="51,000"
+      />
+    </div>
   </>
 );
+
 
